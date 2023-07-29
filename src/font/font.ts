199ff53abe99b8d66ab7e76ee48b8e7d3f54e1ec
Bearei <bear_ei@outlook.com>
2023-07-29 22:48:01 +0800

    feat(font): add font color configuration

--- src/font/font.ts
+++ src/font/font.ts
@@ -9,6 +9,37 @@ const font = {
 
   codeFamily: `JetBrainsMono, source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
   monospace`,
+
+  color: {
+    title: {
+      black: '#000000E0',
+      white: '#FFFFFFD9',
+    },
+    primaryText: {
+      black: '#000000E0',
+      white: '#FFFFFFD9',
+    },
+    secondaryText: {
+      black: '#000000A6',
+      white: '#FFFFFFA6',
+    },
+    disable: {
+      black: '#00000040',
+      white: '#FFFFFF40',
+    },
+    primaryBorder: {
+      black: '#D9D9D9FF',
+      white: '#424242FF',
+    },
+    divider: {
+      black: '#0505050F',
+      white: '#FDFDFD1F',
+    },
+    layoutBackground: {
+      black: '#F5F5F5FF',
+      white: '#000000FF',
+    },
+  },
 };
 
 export default font;
